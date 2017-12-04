29e7a43361a574822a11ab64b5ce6213e77e47e8
lizparody <lizparody@gmail.com>
2017-12-04 13:12:22 -0500

    Made the first test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,18 @@
-import { renderComponent , expect } from '../test_helper';
-import App from '../../src/components/app';
+import { renderComponent, expect } from '../test_helper';
+import App from '../../src/components/app'; // the component the want to test 'app'
 
-describe('App' , () => {
-  let component;
+// Use 'describe' to group together similar test
+describe('App', () => { // App as a string, for the report when we run the test, we will get a report saying what passed and what failed
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+  // Use 'it' to test a single attribute of a target
+  it('shows the correct test', () => {
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+    // Crate an instance of App
+    const component = renderComponent(App)
+
+    // Use 'expect' to make an assertion about a target
+    expect(component).to.contain('React simple starter')
+
+  })
+
+})
