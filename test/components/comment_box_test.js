4e5162b9c3f1f198cca2934df51e90264cdbd3f5
lizparody <lizparody@gmail.com>
2017-12-06 09:47:44 -0500

    Added the beforeEach of every it inside the comment box test

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,20 +2,24 @@ import { renderComponent, expect } from '../test_helper'
 import CommentBox from '../../src/components/comment_box'
 
 describe('CommentBox', () => {
+  let component;
+
+  // The beforeEach runs before every it
+  beforeEach(() => {
+    // It's returning a jquery object that contains our react component (jquery version of our component)
+    component = renderComponent(CommentBox);
+  });
+
   it('has the correct class', () => {
-    const component = renderComponent(CommentBox);
-    expect(component).to.have.class('comment-box')
+    expect(component).to.have.class('comment-box');
   })
 
   it('has a text area', () => {
-    // It's returning a jquery object that contains our react component (jquery version of our component)
-    const component = renderComponent(CommentBox);
     // The text area should exist
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
   });
 });
